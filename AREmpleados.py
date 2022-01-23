48a04939e1c090c75eb077fe1f849c17d29dcf97
ArrZan <joso-eloso@hotmail.com>
2022-01-23 12:53:04 -0500

    Subiendo proyecto 1P - Menú Ficha Personal

---
+++ AREmpleados.py
@@ -0,0 +1,17 @@
+class Empleado:
+    secuencia = 1
+    Empleados= [{"codigo":1,"nombre":"Dan","cedula":"0914192182","cargo":1,"departamento":1,"sueldo":500.50},
+                {"codigo":2,"nombre":"Fernanda","cedula":"0938234252","cargo":2,"departamento":2,"sueldo":1000.00}]
+    
+    def __init__(self,nombre,cedula,codCargo,codDepartamento,sueldo):
+        Empleado.secuencia += 1
+        self.codigo = Empleado.secuencia
+        self.nombre = nombre
+        self.cedula= cedula
+        self.cargo = codCargo
+        self.departamento = codDepartamento
+        self.sueldo = sueldo
+
+    def  registro(self):
+        return {"codigo":self.codigo,"nombre":self.nombre,"cedula":self.cedula,
+        "cargo":self.cargo,"departamento":self.departamento,"sueldo":self.sueldo}
